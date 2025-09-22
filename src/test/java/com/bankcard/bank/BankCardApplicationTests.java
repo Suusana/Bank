49eba1e71001fccd528f3de00368fca039a182e8
Suusana <shenhanhan1@gmail.com>
2025-09-22 11:43:54 +1000

    Initial commit

---
+++ src/test/java/com/bankcard/bank/BankCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankcard.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
